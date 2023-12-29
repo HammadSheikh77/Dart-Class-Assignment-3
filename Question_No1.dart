8addb89173acc0666e6f7c69023ebd1eceeadd9e
MuhammadHammad77 <hammadop74@gmail.com>
2023-12-29 21:29:00 +0500

    main.dart

---
+++ Question_No1.dart
@@ -0,0 +1,11 @@
+// Q1. Write a program that prints the
+// multiplication table of a given number using a for loop.
+
+void main() {
+  //QUESTION NO:1
+
+  var table = 7;
+  for (var i = 1; i <= 10; i++) {
+    print("$table X $i = ${table * i}");
+  }
+}
