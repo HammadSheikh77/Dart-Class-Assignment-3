29a734b287aabd69715770916e01ef1921e5745a
MuhammadHammad77 <hammadop74@gmail.com>
2023-12-30 17:20:10 +0500

    main.dart

--- Question_No4.dart
+++ Question_No4.dart
@@ -5,33 +5,10 @@
 // Otherwise, keep asking for the email and password until the correct credentials
 // are provided.
 
-// import 'dart:io';
-
-// void main() {
-//   //QUESTION NO:4
-//   var isLoggedIn = false;
-//   var attemps = false;
-//   while (isLoggedIn) {
-//     var email = stdin.readLineSync();
-//     var password = stdin.readLineSync();
-//     if (email == "admin@gmail.com" && password == "12345") {
-//       print("Login Successfull");
-//       isLoggedIn = true;
-//     } else {
-//       attemps ++;
-//       if (attemps == 3) {
-//         print("Your Account Has Been Block");
-//         break;
-//       }
-//     }
-//   }
-// }
-
 import 'dart:io';
 
 void main() {
   int attempts = 0;
-
   while (attempts < 3) {
     var email = stdin.readLineSync();
     var password = stdin.readLineSync();
